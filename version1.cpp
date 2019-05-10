cf661ed186c0f587b71d2ce564c187c761606e21
BeroBero16 <ebrammichel16@gmail.com>
2019-05-11 01:48:44 +0200

    Add 1

--- version1.cpp
+++ version1.cpp
@@ -3,16 +3,7 @@ using namespace std ;
 int main()
 {
 
-cout <<"1";
-cout<<"2";
-<<<<<<< HEAD
-cout<<"3";
-cout<<"4";
-cout<<"5";
-cout<<"6";
-=======
-
->>>>>>> parent of cbcc7cf... Add 3
+cout<<"1";
 
 	return 0;
 }
