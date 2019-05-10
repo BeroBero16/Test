3433920e25144179777d4050c2bc13a08d1ca710
BeroBero16 <ebrammichel16@gmail.com>
2019-05-11 01:50:07 +0200

    Revert "Add 1"
    This reverts commit cf661ed186c0f587b71d2ce564c187c761606e21.

--- version1.cpp
+++ version1.cpp
@@ -3,7 +3,16 @@ using namespace std ;
 int main()
 {
 
-cout<<"1";
+cout <<"1";
+cout<<"2";
+<<<<<<< HEAD
+cout<<"3";
+cout<<"4";
+cout<<"5";
+cout<<"6";
+=======
+
+>>>>>>> parent of cbcc7cf... Add 3
 
 	return 0;
 }
