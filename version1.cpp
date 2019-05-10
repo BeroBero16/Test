a46571017f661fffedcd31bd0752818f7f26dade
BeroBero16 <ebrammichel16@gmail.com>
2019-05-11 01:31:05 +0200

    Add 1

--- version1.cpp
+++ version1.cpp
@@ -3,19 +3,7 @@ using namespace std ;
 int main()
 {
 
-cout<<"This is Master";
-// Fun test
-if(true)
-{
-	int i=0;
-}
-
-cout<<"1";
-cout<<"2";
-cout<<"3";
-cout<<"4";
-cout<<"5";
-
+cout <<"1";
 
 
 	return 0;
